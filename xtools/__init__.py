1d8ae5a662866035f579b9f5ace6310a2a16d890
xikasan <xika0907@gmail.com>
2020-04-14 20:48:51 +0900

    dev path collection and directory generation

--- xtools/__init__.py
+++ xtools/__init__.py
@@ -1,1 +1,6 @@
 # coding: utf-8
+
+from .config import *
+from .display import *
+from .path import *
+from .math import *
---
+++ xtools/math.py
@@ -0,0 +1,44 @@
+# coding: utf-8
+
+import numpy as np
+
+
+def d2r(d):
+    if hasattr(d, "__len__"):
+        if isinstance(d, list):
+            return [d2r(de) for de in d]
+        if isinstance(d, tuple):
+            return (d2r(de) for de in d)
+    return d * np.pi / 180
+
+
+def r2d(r):
+    if hasattr(r, "__len__"):
+        if isinstance(r, list):
+            return [r2d(re) for re in r]
+        if isinstance(r, tuple):
+            return (r2d(re) for re in r)
+    return r * 180 / np.pi
+
+
+def __round(x, d=0):
+    p = 10 ** d
+    return float(np.floor((x * p) + np.copysign(0.5, x))) / p
+
+
+def round(x, d=0):
+    if hasattr(x, "__len__"):
+        if isinstance(x, list):
+            return [
+                round(xi, d) for xi in x
+            ]
+        if isinstance(x, tuple):
+            return [
+                round(xi, d) for xi in x
+            ]
+        if isinstance(x, np.ndarray):
+            return np.array([
+                round(xi, d) for xi in x
+            ])
+        raise ValueError("Not supported type: {} is given.".format(type(x)) )
+    return __round(x, d)
